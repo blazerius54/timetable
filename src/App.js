428624be304a0e680a3ed5f8ba7d8a43799b78cb
Maxim Stepanov1 <Maxim_Stepanov1@epam.com>
2019-06-11 13:22:58 +0300

    develop/refactor func comp

--- src/App.js
+++ src/App.js
@@ -1,50 +1,61 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { months } from './data';
 
 function App() {
   const [currentDate, setCurrentDate] = useState(new Date());
-  currentDate.setDate(1);
-  const currentMonth = currentDate.getMonth();
-  const totalDays = new Date(2019, currentMonth + 1, 0).getDate();
-  const elementsToAdd =
-    currentDate.getDay() == 0 ? 6 : currentDate.getDay() - 1;
-  const days = [];
-  let firstWorkDay = 1;
-  let workDays = 2;
-
-  for (let i = 1; i <= totalDays; i++) {
-    days.push({
-      fullDate: new Date(currentDate),
-      workDay: i >= firstWorkDay && workDays > 0,
-    });
-
-    if (i >= firstWorkDay) {
-      workDays--;
-    }
-
-    if (workDays == -2) {
-      workDays = 2;
-    }
-    currentDate.setDate(currentDate.getDate() + 1);
-  }
-
-  for (let i = 0; i < elementsToAdd; i++) {
-    days.unshift({});
-  }
-  console.log(days)
+  const [workDays, setWorkDays] = useState(2);
+  const [days, setDays] = useState([]);
 
   const switchMonth = () => {
-    const dateWithNewMonth = new Date(
-      currentDate.setMonth(currentDate.getMonth()),
-    );
+    const dateWithNewMonth = currentDate;
 
+    currentDate.setMonth(currentDate.getMonth() + 1);
     setCurrentDate(dateWithNewMonth);
+    fillMonth();
   };
+
+  const fillMonth = () => {
+    const newDays = [];
+    const newCurrentDate = new Date(currentDate.setDate(1));
+    const totalDays = new Date(
+      newCurrentDate.getFullYear(),
+      newCurrentDate.getMonth() + 1,
+      0,
+    ).getDate();
+    const elementsToAdd =
+      newCurrentDate.getDay() == 0 ? 6 : newCurrentDate.getDay() - 1;
+    let newWorkDays = workDays;
+
+    for (let i = 0; i < totalDays + elementsToAdd; i++) {
+      if (i < elementsToAdd) {
+        newDays.unshift({});
+      } else {
+        newDays.push({
+          fullDate: new Date(newCurrentDate),
+          workDay: newWorkDays > 0,
+        });
+
+        newCurrentDate.setDate(newCurrentDate.getDate() + 1);
+        newWorkDays--;
+
+        if (newWorkDays == -2) {
+          newWorkDays = 2;
+        }
+      }
+    }
+    setDays(newDays);
+    setWorkDays(newWorkDays);
+  };
+
+  useEffect(() => {
+    fillMonth();
+  }, []);
 
   return (
     <>
       <button onClick={switchMonth}>next</button>
-      Июнь
       <div className="calendar-wrapper">
+        <div className="month-main">{months[currentDate.getMonth()].rus}</div>
         <div className="days-name">
           <div>пн</div>
           <div>вт</div>
@@ -55,22 +66,23 @@ function App() {
           <div>вс</div>
         </div>
         <div className="calendar-body">
-          {days.map(({ fullDate, workDay }) => {
-            if (typeof fullDate === 'object') {
-              return (
-                <div
-                  key={fullDate.getDate()}
-                  className={`day ${workDay && 'workDay'}
-                  ${(fullDate.getDay() == 0 || fullDate.getDay() == 6) &&
-                    'week-end'}
-                  `}
-                >
-                  {fullDate.getDate()}
-                </div>
-              );
-            }
-            return <div className="day" key={Math.random()} />;
-          })}
+          {days.length &&
+            days.map(({ fullDate, workDay }) => {
+              if (fullDate) {
+                const weekDay = fullDate.getDay();
+                const day = fullDate.getDate();
+                return (
+                  <div
+                    key={day}
+                    className={`day ${workDay && 'workDay'}
+                    ${(weekDay == 0 || weekDay == 6) && 'week-end'}`}
+                  >
+                    {day}
+                  </div>
+                );
+              }
+              return <div className="day" key={Math.random()} />;
+            })}
         </div>
       </div>
     </>
